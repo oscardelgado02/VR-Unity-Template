24e42f81cf335c5f862f5e89dd5cd564966b889f
Óscar Delgado <72409291+oscardelgado02@users.noreply.github.com>
2024-05-14 16:30:50 +0200

    Build fix

--- Assets/Scripts/VR-Unity-Template/PhysicsBasedHands/PhysicsHand.cs
+++ Assets/Scripts/VR-Unity-Template/PhysicsBasedHands/PhysicsHand.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PhysicsHand : MonoBehaviour
 {
